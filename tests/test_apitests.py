90e39b5e22bd7a612490773e60e636edc732995f
falfonzo <franciscoj.anguloa@gmail.com>
2026-06-25 18:32:16 +0100

    Fixed APITest

--- tests/test_apitests.py
+++ tests/test_apitests.py
@@ -1,12 +1,14 @@
 import pytest
 from playwright.sync_api import Playwright
 from configurations.config import Config
+from configurations.logger import _logger
 from page_objects.login import LoginPage
 from plawright_clients.orders_client import OrdersClient
 
 config = Config()
 user_mail, user_pass = config.get_credentials_main()
 user_credentials_list = config.get_all_credentials()
+logger = _logger(__name__)
 
 # "user_credentials" is a PARAMETER. user_credentials_list is a variable (stores the credentials)
 
@@ -14,7 +16,7 @@ user_credentials_list = config.get_all_credentials()
 @pytest.mark.parametrize("user_credentials", user_credentials_list)
 # To the Test, you pass "user_credentials" as a FIXTURE (do not confuse with the parameter on top)
 # IMPORTANT: The "user_credentials" fixture must return the parameter for "user_credentials"
-def test_create_order_api(playwright: Playwright, browser_instance, user_credentials, session_logger):
+def test_create_order_api(playwright: Playwright, browser_instance, user_credentials):
     user_mail = user_credentials["email"]
     user_pass = user_credentials["password"]
 
@@ -24,7 +26,7 @@ def test_create_order_api(playwright: Playwright, browser_instance, user_credent
     order_id = OrdersClient(playwright).place_order(user_mail, user_pass)
 
     # PAGE OBJECT MODEL
-    login_page = LoginPage(browser_instance, session_logger)  # Here you pass the fixture. It returns the page.
+    login_page = LoginPage(browser_instance, logger)  # Here you pass the fixture. It returns the page.
     login_page.navigate()  # This will take you to the login page.
 
     login_page.fill_form(user_mail, user_pass)
